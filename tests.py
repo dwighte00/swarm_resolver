c101c7bce93abb3e5413b2cd7cfb92b180a240ac
threathive <adam@threathive.com>
2018-12-27 19:51:14 -0800

    updated tests

--- tests.py
+++ tests.py
@@ -16,13 +16,19 @@ class SwarmTest(unittest.TestCase):
             "thisdomainisnotreal.xyy"
         ]
         self.swarm = SwarmResolver(qtype="NS", num_workers=10)
+        self.swarm2 = SwarmResolver(qtype="A", num_workers=10)
 
 
     def tearDown(self):
         self.swarm = None
+        self.swarm2 = None
 
-    def test_domain_list(self):
+    def test_domain_list_resolve_ns(self):
         f = self.swarm.resolve_list(self.domains)
+        self.assertTrue(f)
+
+    def test_domains_resolve_a(self):
+        f = self.swarm2.resolve_list(self.domains)
         self.assertTrue(f)
 
 if __name__ == "__main__":
